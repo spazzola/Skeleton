f4948d5b2814192a5627cd8aa59d3fb283e71a14
Thomas Smith <P2593135@my365.dmu.ac.uk>
2022-02-11 16:35:11 +0000

    Added testing for clsStock

---
+++ Testing4/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+            clsStock aStock = new clsStock();
+            Assert.IsNotNull(aStock);
+        }
+    }
+}
