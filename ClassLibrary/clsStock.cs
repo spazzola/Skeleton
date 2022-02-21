07bb8cb923da3133e7e12a8c270ee52ffd9c829b
Thomas Smith <P2593135@my365.dmu.ac.uk>
2022-02-21 16:24:29 +0000

    Created properties and tests for all items in database

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,13 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public string ItemDescription { get; set; }
+        public float ItemPrice { get; set; }
+        public int ItemAmount { get; set; }
+        public bool ItemAvailable { get; set; }
+        public DateTime ItemShipment { get; set; }
     }
 }
