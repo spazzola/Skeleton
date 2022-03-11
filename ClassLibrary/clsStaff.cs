ffdb20a20a60428e8111ccf5ce54e6d57bbd994a
P2549877 <P2549877@my365.dmu.ac.uk>
2022-03-11 12:43:57 +0000

    Added find method. Added missing staffId property

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -8,11 +8,86 @@ namespace ClassLibrary
         {
         }
 
-        public string fullName { get; set; }
-        public double salary { get; set; }
-        public string login { get; set; }
-        public string password { get; set; }
-        public bool isAdmin { get; set; }
-        public DateTime lastLogged { get; set; }
+        private Int32 mStaffId;
+        private string mFullName;
+        private double mSalary;
+        private string mLogin;
+        private string mPassword;
+        private bool mIsAdmin;
+        private DateTime mLastLogged;
+
+        public Int32 staffId {
+            get {
+                return mStaffId;
+            }
+            set {
+                mStaffId = value;
+            }
+        }
+
+        public string fullName { 
+            get {
+                return mFullName;
+            }
+            set {
+                mFullName = value;
+            }
+        }
+
+        public double salary {
+            get {
+                return mSalary;
+            }
+            set {
+                mSalary = value;
+            }
+        }
+
+        public string login {
+            get {
+                return mLogin;
+            }
+            set  {
+                mLogin = value;
+            }
+        }
+
+        public string password {
+            get {
+                return mPassword;
+            }
+            set {
+                mPassword = value;
+            }
+        }
+
+        public bool isAdmin {
+            get {
+                return mIsAdmin;
+            }
+            set {
+                mIsAdmin = value;
+            }
+        }
+
+        public DateTime lastLogged {
+            get {
+                return mLastLogged;
+            }
+            set {
+                mLastLogged = value;
+            }
+        }
+
+        public bool Find(int staffId) {
+            mStaffId = 1;
+            mFullName = "Jack Smith";
+            mSalary = 10.0;
+            mLogin = "jack00";
+            mPassword = "smith25";
+            mIsAdmin = true;
+            mLastLogged = Convert.ToDateTime("01/01/2022");
+            return true;
+        }
     }
 }
