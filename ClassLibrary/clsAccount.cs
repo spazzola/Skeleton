d738c1119d0fd4bc93ef03d26785c1cdbbd11fa4
M Meto <30389732+mmmetodiev@users.noreply.github.com>
2022-03-13 10:45:08 +0000

    1st test of clsAccount(tstAccount.cs created)

---
+++ ClassLibrary/clsAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ClassLibrary
+{
+    public class clsAccount
+    {
+        private int id;
+        private string u_name;
+        private string pass;
+        private string email;
+        private bool exist;
+        private DateTime date;
+
+        public clsAccount()
+        {
+            id = 1;
+            u_name = "user";
+            pass = "";
+            email = "";
+            exist = true;
+            date = new DateTime();
+        }
+    }
+}
