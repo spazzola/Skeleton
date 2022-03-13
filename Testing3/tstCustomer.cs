2ae82f843ff1bc0e232b4f19ce60b2536c3d5a97
M Meto <30389732+mmmetodiev@users.noreply.github.com>
2022-03-13 17:32:06 +0000

    after this I was dying internally for 1h straight.

---
+++ Testing3/tstCustomer.cs
@@ -0,0 +1,16 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer acc = new clsCustomer();
+        }
+    }
+}
