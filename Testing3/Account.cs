219a4c56add811555c0ede3e88edda48970056a6
M Meto <30389732+mmmetodiev@users.noreply.github.com>
2022-03-13 09:37:15 +0000

    .mdf file added

---
+++ Testing3/Account.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Account
+{
+    private int id = 0;
+    private string u_name = "";
+    private string pass = "";
+    private bool exist = false;
+    private DateTime acc_created = new DateTime();
+
+
+
+}
---
+++ Testing3/PropertyExists.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Testing3
+{
+	[TestClass]
+	public class PropertyExists
+	{
+		[TestMethod]
+		public void CheckAcctExists()
+		{
+			Account acc = new Account();
+			Boolean test_data = true;
+
+			acc.AccExists = test_data;
+
+			Assert.AreEqual(acc.AccExists, test_data);
+		}
+
+		public void CheckInstanceOK()
+        {
+			Account acc = new Account();
+			Assert.IsNotNull(acc);
+        }
+	}
+}
