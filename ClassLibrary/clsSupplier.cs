2d0d3ffe84d69e6ee6e84e7ef38a6cc41da716b6
rashtocodes <rashto@live.nl>
2022-03-13 22:17:42 +0000

    Added test for Supplier

---
+++ ClassLibrary/clsSupplier.cs
@@ -0,0 +1,11 @@
+﻿namespace ClassLibrary
+{
+    public class clsSupplier
+    {
+        public static string Name { get; set; }
+        public static string Email { get; set; }
+        public int Id { get; set; }
+        public bool Availability { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Testing6/tstSupplier.cs
@@ -0,0 +1,58 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing6
+{
+    [TestClass]
+    public class tstSupplier
+    {
+        [TestMethod]
+        public void InstanceOk()
+        {
+         clsSupplier Supplier = new clsSupplier();
+            Assert.IsNotNull(Supplier);
+        }
+        [TestMethod]
+        public void IdPropertyOk()
+
+        {
+            clsSupplier Supplier = new clsSupplier();
+            int TestData = 1;
+            Supplier.Id = TestData;
+            Assert.AreEqual(Supplier.Id, TestData);
+       
+        }
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            string TestData = "Harry";
+            clsSupplier.Name = TestData;
+            Assert.AreEqual(clsSupplier.Name, TestData);
+        }
+        [TestMethod]
+        public void EmailPropertyOk()
+        {
+            String TestData = "Harry@gmail.com";
+            clsSupplier.Email = TestData;
+            Assert.AreEqual(clsSupplier.Email, TestData);
+        }
+        [TestMethod]
+        public void AvailabilityPropertyOk()
+        {
+            clsSupplier Supplier = new clsSupplier();
+            Boolean TestData = true;
+            Supplier.Availability = TestData;
+            Assert.AreEqual(Supplier.Availability, TestData);
+        }
+        [TestMethod]
+        public void QuantityPropertyOk()
+        {
+            clsSupplier Supplier = new clsSupplier();
+            Int32 TestData = 1;
+            Supplier.Quantity = TestData;
+            Assert.AreEqual(Supplier.Quantity, TestData);
+
+        }
+    }
+}
