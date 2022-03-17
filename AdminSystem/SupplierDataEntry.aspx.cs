5791c60063348b60c15df21854cc0089da55343f
Abdirashid Diriye <P2594540@LEC-ADMIN.dmu.ac.uk>
2022-03-17 00:41:54 +0000

    Revert "Revert "Basic Presentation layer added""
    This reverts commit e0e42dc505f31c833169aa9edcdf6780cf744f3e.

--- AdminSystem/SupplierDataEntry.aspx.cs
+++ AdminSystem/SupplierDataEntry.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -11,4 +12,17 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    protected void btnOk_Click(object sender, EventArgs e)
+
+    {
+        clsSupplier Supplier = new clsSupplier();
+        clsSupplier.Name = txtName.Text;
+        Session["Supplier"] = Supplier;
+        Response.Redirect("SupplierViewer.aspx");
+  
+    }
+
+
 }
+
--- AdminSystem/SupplierViewer.aspx.cs
+++ AdminSystem/SupplierViewer.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
-public partial class _1Viewer : System.Web.UI.Page
+public partial class _SupplierViewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsSupplier Supplier = new clsSupplier();
+        Supplier = (clsSupplier)Session["Supplier"];
+        Response.Write(Supplier.Id);
     }
 }
