0eb65e368260aa8c25852246e49405b691468bfc
rashtocodes <98796247+rashtocodes@users.noreply.github.com>
2022-03-26 07:14:19 +0000

    Update clsSupplier.cs

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -4,8 +4,9 @@
     {
         public static string Name { get; set; }
         public static string Email { get; set; }
-        public int Id { get; set; }
+        public int SupplierId { get; set; }
         public bool Availability { get; set; }
         public int Quantity { get; set; }
+        public static string ProductDescription { get; set }
     }
 }
