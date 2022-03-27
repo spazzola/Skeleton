28d13c8dcb103a12af267b0534eb8bce2242e851
Maksimiliyan Metodiev <P2583968@my365.dmu.ac.uk>
2022-03-27 13:05:52 +0100

    more changes

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
